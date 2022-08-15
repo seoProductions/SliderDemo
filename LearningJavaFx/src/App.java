773830f10b51681874fbddeb2ab15a9412ee03a8
seoProductions <eduque135@gmail.com>
2022-08-14 21:39:09 -0500

    testing

--- LearningJavaFx/src/App.java
+++ LearningJavaFx/src/App.java
@@ -21,6 +21,7 @@ public class App extends Application{
         currentStage.setScene(scene);
         currentStage.show();
 
+        //cool stuff
     }
 
     public static void main(String[] args) throws Exception {
